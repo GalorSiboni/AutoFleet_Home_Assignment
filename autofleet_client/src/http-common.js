1b0bc0d6072d7b2915d96a8e4eec6353f9c4a379
Galor Siboni <galorsiboni@gmail.com>
2021-11-09 06:41:45 +0200

    removing port from url

--- autofleet_client/src/http-common.js
+++ autofleet_client/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://nodejs-autofleet-server.herokuapp.com" + (process.env.PORT || 5000) + "/api",
+    baseURL: "https://nodejs-autofleet-server.herokuapp.com/api",
     headers: {
         "Accept": "application/json", "Content-type": "application/json", 'Access-Control-Allow-Origin': '*',
         common:{'Authorization': 'Bearer'}
